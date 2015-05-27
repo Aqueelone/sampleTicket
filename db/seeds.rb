f7ee1f22658d336dad82e1b459a92a251a1a0305
Sergy Nazarevich <snazarevich@chegg.com>
2015-05-27 17:05:46 +0300

    ++ add demo data with windget rules and widgets

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,3 +16,24 @@ TicketStatus.create([
                 {name: "Aproving P1", position: 4},
                 {name: "Closed", position: 6}
               ])
+WidgetRule.create([
+    {controlled_id: 1, controlled_type: "TicketStatus"},
+    {controlled_id: 4, controlled_type: "Category"},
+    {controlled_id: 3, controlled_type: "Category"},
+    {controlled_id: 1, controlled_type: "Category"},
+    {controlled_id: 2, controlled_type: "Category"},
+    {controlled_id: 2, controlled_type: "TicketStatus"},
+    {controlled_id: 3, controlled_type: "TicketStatus"},
+    {controlled_id: 5, controlled_type: "TicketStatus"},
+    {controlled_id: 4, controlled_type: "TicketStatus"},
+    {controlled_id: 6, controlled_type: "TicketStatus"}
+  ])
+
+Widget.create([
+{name: "[template:] Waiting for Staff Response", is_template: true, is_readonly: true, widget_rule_ids: [1, 2, 3, 4, 5, 6, 7]},
+{name: "[template:] On hold", is_template: true, is_readonly: true, widget_rule_ids: [1, 3, 4, 5, 8, 9]},
+{name: "Waiting for Staff Response", template_id: 1, is_readonly: true, widget_rule_ids: [1, 2, 3, 4, 5, 6, 7]},
+{name: "[template:] Solved issues", is_template: true, is_readonly: true, widget_rule_ids: [1, 3, 4, 5, 10]},
+{name: "Solved issues", template_id: 4, is_readonly: true, widget_rule_ids: [1, 3, 4, 5, 10]},
+{name: "On hold", template_id: 2, is_readonly: true, widget_rule_ids: [1, 3, 4, 5, 8, 9]},
+  ])
